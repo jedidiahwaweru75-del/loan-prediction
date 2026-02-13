bd4f11d1254932e8749bc1d9eb51d2c7f970f82f
jedidiahwaweru75-del <jedidiahwaweru75@gmail.com>
2026-02-13 21:35:30 +0300

    loaded and explored data

--- codes.py
+++ codes.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+# loading data 
+data=pd.read_csv('data/trainperf.csv')
+
+#view first rows
+data.head()
+#view first columns
+data.tail()
+#check number of rows and columns
+data.shape
+#see column names
+data.columns
+#get full information
+data.info()
+#summary statistics
+data.describe()
